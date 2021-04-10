ff8c2cd45aa0b271605ac3a0dbcb02444b0c204a
Raad Saleh <xblaketrixx@gmail.com>
2021-04-10 23:08:41 +0600

    raad er kaam

---
+++ Controller/crudhousevalidation.php
@@ -0,0 +1,103 @@
+<?php
+include "DB/db_config.php";
+$conn = mysqli_connect($server,$user,$pass,$db);
+
+
+$query = "SELECT * FROM house";
+$result=mysqli_query($conn,$query);
+if(isset($_POST["showBtn"]))
+{
+    $result=mysqli_query($conn,$query);
+}
+
+
+$id="";
+$err_id="";
+$owner="";
+$err_owner="";
+$location="";
+$err_location="";
+$contact="";
+$err_contact="";
+$price="";
+$size="";
+$err_price="";
+$err_size="";
+$hasError=false;
+
+if(isset($_POST["addBtn"]))
+{
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	
+	if(empty($_POST["owner"])){
+    $err_owner="[insert owner name]";
+    $hasError=true;
+}
+
+if(empty($_POST["location"])){
+    $err_location="[insert location]";
+    $hasError=true;
+}
+	
+	
+	if (!is_numeric($_POST["price"])) {
+        
+        $err_price = "[This field is empty or requires only numeric values]";
+        $hasError = true;
+    }
+	
+    else{
+        $price=htmlspecialchars($_POST["price"]);
+    }
+	if (!is_numeric($_POST["size"])) {
+        
+        $err_size = "[This field is empty or requires only numeric values]";
+        $hasError = true;
+    }
+    else{
+        $size=htmlspecialchars($_POST["size"]);
+    }
+	if (!is_numeric($_POST["contact"])) {
+        
+        $err_contact = "[This field is empty or requires only numeric values]";
+        $hasError = true;
+    }
+    else{
+        $contact=htmlspecialchars($_POST["contact"]);
+    }
+	
+	$owner = $_POST['owner'];
+	$price = $_POST['price'];
+	$location = $_POST['location'];
+	$size = $_POST['size'];
+	$contact = $_POST['contact'];
+	
+  if($hasError === false){
+    $insert_query = "insert into house values ('$id','$owner','$price','$location','$size','$contact')";
+    if(mysqli_query($conn,$insert_query)){
+        echo "values inserted";
+		
+$id="";
+$err_id="";
+$owner="";
+$err_owner="";
+$location="";
+$err_location="";
+$contact="";
+$err_contact="";
+$price="";
+$size="";
+$err_price="";
+$err_size="";
+
+
+    }
+    else{
+        echo "problem not inserted";
+    }
+  }
+}
+}
+
+?>
---
+++ homepagetenant.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<title>Home</title>
+   <head>
+   <link rel="stylesheet" href="homepage.css">
+
+<body>
+
+  <div class="topnav">
+  <a href=http://localhost/homepage/search.php>Search</a>
+  <a href="#req">Profile</a>
+  <a href="#req">Payment</a>
+  <a href="#req">Request for rent</a>
+  
+  
+  <div style="text-align:right;"> <input type="submit" class="my-font btn-sup" name="Logout" value="log out"></div>
+  
+</div>
+	 
+  
+	
+	 <p> 
+	  <div class="topnav">
+	  <legend style="text-align:center;"><h1 style="color:orange" ></h1></legend>
+	  <p></p>
+	  <p></p>
+	  <p></p>
+	  <p></p>
+	  </div>
+	  <p>
+	  <div class="tren">
+	  <p>.</p>
+	  <p>.</p>
+	  <p>.</p>
+	  <p>.</p>
+	  <p>.</p>
+	  <p>.</p>
+	  </div>
+     </p>
+	 </p>
+	 </body>
+   </head>
+   
+</html>
