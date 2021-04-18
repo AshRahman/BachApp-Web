9b5f05fa1eb8586656d0d419dd24b2d973b8f587
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-04-18 23:44:52 +0600

    PHP fromatting changed

--- adminaddbachelor.php
+++ adminaddbachelor.php
@@ -1,102 +1,103 @@
-
 <?php
-       include "Controller/adminaddbachelorValidation.php";
-       include "Controller/logoutValidation.php";
-	session_start(); 
-	if(!isset($_SESSION["user_type"])){
-		header("Location:userError.php");
-	}
-        
- ?>
-    
+include "Controller/adminaddbachelorValidation.php";
+include "Controller/logoutValidation.php";
+session_start();
+if (!isset($_SESSION["user_type"])) {
+    header("Location:userError.php");
+}
+
+?>
+
 
 
 
 <html>
+
 <head>
 </head>
+
 <body>
-<div class="right">
+    <div class="right">
         <form action="" method="post">
-        <input type="submit" class="right my-font btn-sup" name="logoutBtn" value="Log-out"> 
-        </form> 
+            <input type="submit" class="right my-font btn-sup" name="logoutBtn" value="Log-out">
+        </form>
 
         <form action="homepageadmin.php">
-        <input type="submit" class="backBtn" id="backBtn" name="backBtn" value="Go-Back">
+            <input type="submit" class="backBtn" id="backBtn" name="backBtn" value="Go-Back">
         </form>
-        
-        <div id="add-div" >
-        <h1 align="center">Add Bachelor Profile</h1>
 
-        <form action="" onsubmit="return validate()" method="post">
-            <table align="center">
-                <tr>
-                    <td><span>Name</span></td>
-                </tr>
-                <tr>
+        <div id="add-div">
+            <h1 align="center">Add Bachelor Profile</h1>
 
-                    <td><input type="text" id="name" name="name" value="<?php echo $name; ?>" placeholder="<?php echo $name; ?>">
-                        <span id="err_name"><?php echo $err_name; ?></span></br>
-                    </td>
+            <form action="" onsubmit="return validate()" method="post">
+                <table align="center">
+                    <tr>
+                        <td><span>Name</span></td>
+                    </tr>
+                    <tr>
 
-                </tr>
+                        <td><input type="text" id="name" name="name" value="<?php echo $name; ?>" placeholder="<?php echo $name; ?>">
+                            <span id="err_name"><?php echo $err_name; ?></span></br>
+                        </td>
 
-                <tr>
-                    <td><span>User Name</span></td>
-                </tr>
-                <tr>
+                    </tr>
 
-                    <td><input type="text" id="user_name" name="user_name" value="<?php echo $user_name; ?>" placeholder="<?php echo $user_name; ?>">
-                        <span id="err_user_name"><?php echo $err_user_name; ?></span></br>
-                    </td>
+                    <tr>
+                        <td><span>User Name</span></td>
+                    </tr>
+                    <tr>
 
-                </tr>
+                        <td><input type="text" id="user_name" name="user_name" value="<?php echo $user_name; ?>" placeholder="<?php echo $user_name; ?>">
+                            <span id="err_user_name"><?php echo $err_user_name; ?></span></br>
+                        </td>
 
-                <tr>
-                    <td><span>Password</span></td>
-                </tr>
-                <tr>
-                    <td><input type="password" id="pass" name="pass" value="<?php echo $pass; ?>" placeholder="<?php echo $pass; ?>">
-                        <span id="err_pass"><?php echo $err_pass; ?></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td><span>Gender<span></td>
-                </tr>
-                <tr>
-                    <td><input type="radio" id="gender" name="gender" value="Male">Male
-                        <input type="radio" id="gender" name="gender" value="Female">Female
-                        <span id="err_gender"><?php echo $err_gender; ?></span>
-                    </td>
+                    </tr>
 
-                </tr>
+                    <tr>
+                        <td><span>Password</span></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" id="pass" name="pass" value="<?php echo $pass; ?>" placeholder="<?php echo $pass; ?>">
+                            <span id="err_pass"><?php echo $err_pass; ?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><span>Gender<span></td>
+                    </tr>
+                    <tr>
+                        <td><input type="radio" id="gender" name="gender" value="Male">Male
+                            <input type="radio" id="gender" name="gender" value="Female">Female
+                            <span id="err_gender"><?php echo $err_gender; ?></span>
+                        </td>
 
-                <tr>
-                    <td><span>E-mail</span></td>
-                </tr>
-                <tr>
-                    <td><input type="text" id="email" name="email" value="<?php echo $email; ?>" >
-                        <span id="err_email"><?php echo $err_email; ?></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td><span>Contact Number</span></td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="text" id="contact_number" name="contact_number" value="<?php echo $contact_number; ?>"
-                            placeholder="<?php echo $contact_number; ?>">
-                        <span id="err_contact_number"><?php echo $err_contact_number; ?></span>
-                    </td>
-                </tr>
-             
-                <tr>
-                    <td colspan="2"><input type="submit" class="add-Btn" id="add-Btn" name="add-Btn" value="Add"></td>
-                            
-                </tr>
-            </table>
-        </form>
-    </div>
+                    </tr>
+
+                    <tr>
+                        <td><span>E-mail</span></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" id="email" name="email" value="<?php echo $email; ?>">
+                            <span id="err_email"><?php echo $err_email; ?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><span>Contact Number</span></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="text" id="contact_number" name="contact_number" value="<?php echo $contact_number; ?>" placeholder="<?php echo $contact_number; ?>">
+                            <span id="err_contact_number"><?php echo $err_contact_number; ?></span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2"><input type="submit" class="add-Btn" id="add-Btn" name="add-Btn" value="Add"></td>
+
+                    </tr>
+                </table>
+            </form>
+        </div>
 </body>
 <script src="JS/adminValidation.js"></script>
+
 </html>
