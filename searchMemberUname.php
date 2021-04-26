d6ca5eced6f039dbcf531cd0e9f0ef786620f357
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-04-27 00:21:25 +0600

    slight

--- searchMemberUname.php
+++ searchMemberUname.php
@@ -1,8 +1,8 @@
-<?php
-    require_once 'Controller/adminsearchmemberValidation.php';
+<!-- <?php
+    /*require_once 'Controller/adminsearchmemberValidation.php';
     $key=$_GET["key"];
     $username=searchMemberUname($key);
     foreach($username as $usernames){
-        echo $username["username"]."<br>";
+        echo $usernames["username"]."<br>";
     }
-?>
+?> -->
